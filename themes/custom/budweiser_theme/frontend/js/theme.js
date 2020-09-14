bcb51cdddd600b6d99c080d450da3638ecb61f10
edwinmesa <edwinmesa@MacBook-Pro-de-edwinmesa.local>
2020-09-14 15:32:02 -0500

    styles error

--- themes/custom/budweiser_theme/frontend/js/theme.js
+++ themes/custom/budweiser_theme/frontend/js/theme.js
@@ -1,7 +1,48 @@
-jQuery(document).ready(function($) {
-    if ($("body").hasClass("landing-bacaneria")) {
-        $(window).on('load',function(){
-            $('#myModal').modal('show');
+(function ($, Drupal) {
+  Drupal.behaviors.registry = {
+    attach: function (context, settings) {
+      $.validator.addMethod(
+        "customphone",
+        function (value, element) {
+          return (
+            this.optional(element) ||
+            /^(3-?)?(\([0-9]\d{2}\)|[0-9]\d{2})-?[0-9]\d{2}-?\d{4}$/.test(value)
+          );
+        },
+        "Número de celular no  válido"
+      );
+
+
+        $("#user-register-form").validate({
+
+          rules: {
+            'field_celular[0][value]': "customphone",
+          },
         });
-    }
-})
+
+
+
+      let userRegister = document.querySelector(".user-register-form");
+      console.log(userRegister);
+
+     /*  if (userRegister.length >0) {
+        console.log("hola ricky ");
+        $(".user-register-form").validate({
+          submitHandler: function (form) {
+            var formId = form.id;
+            if (!Drupal.agegateValidate.submit) {
+            $("#edit-field-celular-0-value").validate({
+              rules: {
+                nomTelefono: "customphone",
+              },
+            });
+            } else {
+              return false;
+            }
+          },
+        });
+      } */
+    },
+  };
+
+})(jQuery, Drupal);
