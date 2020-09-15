7e55b4937b171befb74c79d74e04e9aff909e889
edwinmesa <edwinmesa@MacBook-Pro-de-edwinmesa.local>
2020-09-15 12:14:18 -0500

    agegate row-3

--- themes/custom/budweiser_theme/frontend/js/theme.js
+++ themes/custom/budweiser_theme/frontend/js/theme.js
@@ -1,27 +1,39 @@
 (function ($, Drupal) {
   Drupal.behaviors.registry = {
     attach: function (context, settings) {
-       jQuery.extend(jQuery.validator.messages, {
-          required: "Campo obligatorio",
-          lettersonly: "Por favor, rellena este campo solo con letras.",
-          remote: "Por favor, rellena este campo.",
-          email: "Por favor, escribe una dirección de correo válida.",
-          url: "Por favor, escribe una URL válida.",
-          date: "Por favor, escribe una fecha válida.",
-          dateITA: "Por favor, escribe una fecha válida.",
-          dateISO: "Por favor, escribe una fecha (ISO) válida.",
-          number: "Por favor, escribe un número válido.",
-          digits: "Por favor, escribe sólo dígitos.",
-          creditcard: "Por favor, escribe un número de tarjeta válido.",
-          equalTo: "Por favor, escribe el mismo valor de nuevo.",
-          extension: "Por favor, escribe un valor con una extensión aceptada.",
-          maxlength: $.validator.format("Por favor, no escribas más de {0} dígitos."),
-          minlength: $.validator.format("Por favor, no escribas menos de {0} dígitos."),
-          rangelength: $.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
-          range: $.validator.format("Por favor, escribe un valor entre {0} y {1}."),
-          max: $.validator.format("Por favor, escribe un valor menor o igual a {0}."),
-          min: $.validator.format("Por favor, escribe un valor mayor o igual a {0}."),
-        });
+      jQuery.extend(jQuery.validator.messages, {
+        required: "Campo obligatorio",
+        lettersonly: "Por favor, rellena este campo solo con letras.",
+        remote: "Por favor, rellena este campo.",
+        email: "Por favor, escribe una dirección de correo válida.",
+        url: "Por favor, escribe una URL válida.",
+        date: "Por favor, escribe una fecha válida.",
+        dateITA: "Por favor, escribe una fecha válida.",
+        dateISO: "Por favor, escribe una fecha (ISO) válida.",
+        number: "Por favor, escribe un número válido.",
+        digits: "Por favor, escribe sólo dígitos.",
+        creditcard: "Por favor, escribe un número de tarjeta válido.",
+        equalTo: "Por favor, escribe el mismo valor de nuevo.",
+        extension: "Por favor, escribe un valor con una extensión aceptada.",
+        maxlength: $.validator.format(
+          "Por favor, no escribas más de {0} dígitos."
+        ),
+        minlength: $.validator.format(
+          "Por favor, no escribas menos de {0} dígitos."
+        ),
+        rangelength: $.validator.format(
+          "Por favor, escribe un valor entre {0} y {1} caracteres."
+        ),
+        range: $.validator.format(
+          "Por favor, escribe un valor entre {0} y {1}."
+        ),
+        max: $.validator.format(
+          "Por favor, escribe un valor menor o igual a {0}."
+        ),
+        min: $.validator.format(
+          "Por favor, escribe un valor mayor o igual a {0}."
+        ),
+      });
       $.validator.addMethod(
         "customphone",
         function (value, element) {
@@ -32,9 +44,19 @@
       $("#user-register-form").validate({
         rules: {
           "field_celular[0][value]": "customphone",
+          "field_terminos[value]": {
+            required: true,
+            maxlength: 2,
+          },
           "field_identificacion[0][value]": {
             number: true,
-          }
+          },
+        },
+        messages: {
+          "field_terminos[value]": {
+            required: "You must check at least 1 box",
+            maxlength: "Check no more than {0} boxes",
+          },
         },
       });
     },
